69313d1f1b6917129888fb1a58d8ccaf59850868
Iqmal <iqmalhazeeq321@gmail.com>
2025-12-06 23:59:16 +0900

    add mood_grouping

---
+++ test.py
@@ -0,0 +1,16 @@
+mood_grouping = {
+    "happy" : ["happy", "amazing", "wonderful", "excited", "contented", "cheerful", "cheery", 
+              "joyful", "jolly", "gleeful", "delighted", "smiling", "beaming", "grinning", 
+              "glowing", "satisfied", "blithe", "joyous", "beatific", "blessed", "thrilled", 
+              "elated", "exhilarated", "ecstatic", "blissful", "euphoric", "overjoyed", 
+              "on cloud nine", "over the moon", "glad", "fortunate", "lucky", "favourable",
+               "good", "right"],
+
+    "sad"   : ["sad", "terrible", "depressed", "unhappy", "sorrowful", "dejected", "miserable", "low",
+               "down", "gloomy", "blue", "melancholy", "melancholic", "low-spirited", "heartbroken",
+               "awful", "wretched", "sorry", "pitiful", "upset", "pathetic", "shameful", "dreadful"],
+
+    "angry" : ["angry", "furious", "mad", "annoyed", "irritated", "displeased", "provoked", "resentful",
+               "enraged", "fuming", "outraged", "bad-tempered", "hot-tempered", "short-tempered", 
+               "riled", "pissed"]
+}
